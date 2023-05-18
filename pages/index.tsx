8f7db57817f2a0248f63d4d0955ab23f4a21ffaf
chae <chae@chaeui-MacBookPro.local>
2023-05-18 23:02:07 +0900

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Login Page</title>
+        <title>로그인 페이지</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
